550069020eaa8a79978744900ac40114f6d6cd36
vusechep <134221541+vusechep@users.noreply.github.com>
2025-09-06 21:52:31 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([4.61627462043914, -74.16101352284693], 13);
+var map = L.map('map').setView([4.610587189482015, -74.11260430153935], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([4.61627462043914, -74.16101352284693]).addTo(map);
+var marker = L.marker([4.610587189482015, -74.11260430153935]).addTo(map);
